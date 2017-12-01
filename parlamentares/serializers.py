2efb383e580bbe2cb7d3785161af008a072c3751
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-12-01 07:32:46 -0200

    Serializer updates and saves children

--- parlamentares/serializers.py
+++ parlamentares/serializers.py
@@ -12,10 +12,69 @@ class LegislaturaSerializer(serializers.ModelSerializer):
         model = models.Legislatura
         fields = ('id', 'numero', 'inicio', 'fim')
 
+class SuplenteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Suplente
+        fields = ('id', 'descricao_participacao', 'parlamentar_id')
+
+class ExercicioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Exercicio
+        fields = ('id', 'inicio', 'leitura', 'causa_afastamento')
+        extra_kwargs = {
+            'id': {'validators': []},
+        }
+
 class MandatoSerializer(serializers.ModelSerializer):
     legislaturas = LegislaturaSerializer(many=True)
+    suplentes = SuplenteSerializer(many=True)
+    exercicios = ExercicioSerializer(many=True)
 
     class Meta:
         model = models.Mandato
-        fields = ('id', 'participacao', 'legislaturas')
+        fields = ('id', 'participacao', 'legislaturas', 'suplentes', 'exercicios')
         depth = 2
+
+    def _create_inner_object(self, model, mandato, rows):
+        for data in rows:
+            model.objects.create(mandato=mandato, **data)
+
+    def _create_exercicio(self, mandato, exercicios):
+        for exercicio in exercicios:
+            if models.Exercicio.objects.filter(id=exercicio['id']).exists():
+                models.Exercicio.objects.filter(id=exercicio['id']).update(**exercicio)
+            else:
+                models.Exercicio.objects.create(mandato=mandato, **exercicio)
+
+    def create(self, validated_data):
+        legislaturas = validated_data.pop('legislaturas')
+        suplentes = validated_data.pop('suplentes')
+        exercicios = validated_data.pop('exercicios')
+
+        mandato = models.Mandato.objects.create(**validated_data)
+
+        self._create_inner_object(models.Legislatura, mandato, legislaturas)
+        self._create_inner_object(models.Suplente, mandato, suplentes)
+        self._create_exercicio(mandato, exercicios)
+        # self._create_inner_object(models.Exercicio, mandato, exercicios)
+
+        return mandato
+
+    def update(self, instance, validated_data):        
+        legislaturas = validated_data.pop('legislaturas')
+        suplentes = validated_data.pop('suplentes')
+        exercicios = validated_data.pop('exercicios')
+
+        instance.legislaturas.all().delete()
+        instance.suplentes.all().delete()
+        # instance.exercicios.all().delete()
+
+        self._create_inner_object(models.Legislatura, instance, legislaturas)
+        self._create_inner_object(models.Suplente, instance, suplentes)
+        # self._create_inner_object(models.Exercicio, instance, exercicios)
+        self._create_exercicio(instance, exercicios)
+
+        # instance.update(**validated_data)
+        models.Mandato.objects.filter(pk=validated_data['id']).update(**validated_data)
+
+        return instance
--- parlamentares/services.py
+++ parlamentares/services.py
@@ -19,35 +19,34 @@ class SyncSenadoresEmAtividadeService:
 
     def _sync(self, data):
         for parlamentar_root in data:
-            data = self._map_mandato(parlamentar_root['Mandato'])
-            mandato = self._get_or_create(
-                data, 
-                models.Mandato,
-                MandatoSerializer
-            )
+            try:
+                data = self._map_mandato(parlamentar_root['Mandato'])
+                mandato = self._get_or_create(
+                    data, 
+                    models.Mandato,
+                    MandatoSerializer
+                )
 
-            data = self._map_parlamentar(parlamentar_root['IdentificacaoParlamentar'])
-            parlamentar = self._get_or_create(
-                data,
-                models.Parlamentar,
-                ParlamentarSerializer,
-                mandato=mandato
-            )
-
-            print('ok')
+                data = self._map_parlamentar(parlamentar_root['IdentificacaoParlamentar'])
+                parlamentar = self._get_or_create(
+                    data,
+                    models.Parlamentar,
+                    ParlamentarSerializer,
+                    mandato=mandato
+                )
+                print('ok')
+            except Exception as excp:
+                print(excp)
 
     def _get_or_create(self, data, entity, serializer_cls, **kwargs):
-        try:
-            instance = None
-            if entity.objects.filter(id=data['id']).exists():
-                instance = entity.objects.get(id=data['id'])
+        instance = None
+        if entity.objects.filter(id=data['id']).exists():
+            instance = entity.objects.get(id=data['id'])
 
-            serializer = serializer_cls(instance, data=data)
-            serializer.is_valid(raise_exception=True)
-            saved = serializer.save(**kwargs)
-            return saved
-        except Exception as exp:
-            print(exp)
+        serializer = serializer_cls(instance, data=data)
+        serializer.is_valid(raise_exception=True)
+        saved = serializer.save(**kwargs)
+        return saved
 
     def _map_parlamentar(self, raw):
         return {
@@ -70,10 +69,12 @@ class SyncSenadoresEmAtividadeService:
         return {
             'id': raw['CodigoMandato'],
             'participacao': raw['DescricaoParticipacao'],
-            'legislaturas': self._map_legislatura_list(raw)
+            'legislaturas': self._map_legislaturas(raw),
+            'suplentes': self._map_suplentes(raw['Suplentes']['Suplente']),
+            'exercicios': self._map_exercicios(raw['Exercicios']['Exercicio'])
         }
 
-    def _map_legislatura_list(self, raw):
+    def _map_legislaturas(self, raw):
         return [
             self._map_legislatura(raw['PrimeiraLegislaturaDoMandato']),
             self._map_legislatura(raw['SegundaLegislaturaDoMandato'])
@@ -84,4 +85,31 @@ class SyncSenadoresEmAtividadeService:
             'numero': raw['NumeroLegislatura'],
             'inicio': raw['DataInicio'],
             'fim': raw['DataFim']
+        }
+
+    def _map_suplentes(self, raw):
+        if isinstance(raw, list):
+            return [self._map_suplente(x) for x in raw]
+        else:
+            return [self._map_suplente(raw)]
+        
+
+    def _map_suplente(self, raw):
+        return {
+            'descricao_participacao': raw['DescricaoParticipacao'],
+            'parlamentar_id': raw['CodigoParlamentar']            
+        }
+
+    def _map_exercicios(self, raw):
+        if isinstance(raw, list):
+            return [self._map_exercicio(x) for x in raw]
+        else:
+            return [self._map_exercicio(raw)]
+
+    def _map_exercicio(self, raw):
+        return {
+            'id': raw['CodigoExercicio'],
+            'inicio': raw['DataInicio'],
+            'leitura': raw.get('DataLeitura', None),
+            'causa_afastamento': raw.get('DescricaoCausaAfastamento', '')
         }
